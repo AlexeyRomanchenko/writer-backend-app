a09f2ca4faa865070181c47c9067278911c66bca
Cici Woolf <christinemarie.woolf>
2020-09-21 09:07:41 -0300

    Added Elements and Components Folders

---
+++ my/src/elements/CardElements.js
@@ -0,0 +1,10 @@
+import styled from "style-components"
+
+export const Cardwrapper = styled.div`
+    margin-top: 2 rem;
+    padding: 1 rem;
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+`
+
