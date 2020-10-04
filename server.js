97eb2d20c51813dfbf1f8949980935374aaa2e3e
alexey.romanchenko <romanchenko.alek@mail.ru>
2020-10-04 23:14:38 +0300

    add aws configuration

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get("/", (req, res) => {
   res.render("index.html");
 });
 
+app.get("/card/info", (req, res) => {
+  let info = {
+    header: "Wincent Wickoff",
+    content:
+      "Olutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellu.",
+    quotes:
+      "“The world needs more wise and gentle voices like Vince Wyckoff’s.” - Lorna Landvik",
+  };
+  res.json(info);
+});
+
 app.get("/about", (req, res) => {
   //throw new Error("Went wrong try again later");
   res.send("About");
