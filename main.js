497d7e062826860dba501d622d7adc2a9b2661a3
alexey.romanchenko <romanchenko.alek@mail.ru>
2020-10-14 20:34:20 +0300

    add cors

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 
